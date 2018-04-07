ac5c04bfab002dc31e6cc991348b6a5b87f6b7f1
Brad Campbell <grandstaish@gmail.com>
2018-04-07 18:08:09 +0100

    Add initial oauth plugin for iOS

---
+++ ios/Runner/OauthPlugin.swift
@@ -0,0 +1,41 @@
+//
+//  OauthPlugin.swift
+//  Runner
+//
+//  Created by Brad Campbell on 07/04/2018.
+//  Copyright © 2018 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+import Flutter
+
+public class OauthPlugin: NSObject, FlutterPlugin {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.monzo/oauthPlugin", binaryMessenger: registrar.messenger())
+        let plugin = OauthPlugin(channel: channel)
+        registrar.addMethodCallDelegate(plugin, channel: channel)
+    }
+    
+    private var oauthChannel: FlutterMethodChannel?
+    private var url: URL?
+    
+    init(channel: FlutterMethodChannel) {
+        oauthChannel = channel
+        super.init()
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "setUrl":
+            let arguments = call.arguments as! Dictionary<String, Any?>
+            url = URL(string: arguments["url"] as! String)
+        case "launch":
+            guard let url = url else {
+                fatalError("url must be set before calling launch")
+            }
+            UIApplication.shared.openURL(url)
+        default:
+            break;
+        }
+    }
+}
